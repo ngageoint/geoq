e4224988bb4d4f0a9560a68f0d2b973370ffdd92
jackiekazil <jackiekazil@gmail.com>
2014-04-29 21:14:19 -0400

    Minor admin.py file clean up.

--- geoq/core/middleware.py
+++ geoq/core/middleware.py
@@ -2,6 +2,7 @@
 # This technical data was produced for the U. S. Government under Contract No. W15P7T-13-C-F600, and
 # is subject to the Rights in Technical Data-Noncommercial Items clause at DFARS 252.227-7013 (FEB 2012)
 
+
 class UserPermsMiddleware(object):
 
     def process_request(self, request):
