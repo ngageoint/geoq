46c9c89b41437132df4da40a1e2549c376bdd521
Stephen Jones <srjones@mitre.org>
2014-05-01 15:36:53 -0400

    MGRS service. Note, this requires 'GeoConvert' application in PATH of local system

---
+++ geoq/mgrs/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# This technical data was produced for the U. S. Government under Contract No. W15P7T-13-C-F600, and
+# is subject to the Rights in Technical Data-Noncommercial Items clause at DFARS 252.227-7013 (FEB 2012)
