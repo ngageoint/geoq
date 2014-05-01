941362f86f5a87094bdcfd86bf04a718035c732a
jackiekazil <jackiekazil@gmail.com>
2014-05-01 11:01:27 -0400

    Adding quick seach on name and id to aoi admin page.

--- geoq/core/admin.py
+++ geoq/core/admin.py
@@ -19,6 +19,7 @@ class AOIAdmin(ObjectAdmin):
     filter_horizontal = ("reviewers",)
     save_on_top = True
     actions = ['rename_aois']
+    search_fields = ['name','id']
 
     class NameInputForm(forms.Form):
         _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
