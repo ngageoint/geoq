51a228b780e5615dbcc18f8744af4335cdaa426d
jcrossler <jay.crossler@gmail.com>
2014-05-10 14:20:46 -0400

    Show version number in dropdown on top right

---
+++ geoq/core/templatetags/version.py
@@ -0,0 +1,9 @@
+from django import template
+import time
+import os
+
+register = template.Library()
+
+@register.simple_tag
+def version_date():
+    return time.strftime('%m/%d/%Y', time.gmtime(os.path.getmtime('../.git')))
