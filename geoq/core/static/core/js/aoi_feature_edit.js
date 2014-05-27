efcd38c2a7cff3ce47b360f5501ce4f58ea22180
swohlever <wohlever@mitre.org>
2014-05-27 13:54:14 -0400

    Restored another previous fix clobbered by a merge.

--- geoq/core/static/core/js/aoi_feature_edit.js
+++ geoq/core/static/core/js/aoi_feature_edit.js
@@ -353,7 +353,8 @@ aoi_feature_edit.buildDrawingControl = function (drawnItems) {
             //}
         },
         edit: {
-            featureGroup: drawnItems
+            featureGroup: drawnItems,
+            remove: false
         }
     });
 
