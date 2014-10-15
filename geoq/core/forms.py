9d8f85385aa9672061db9cadd8363ee657100a50
Stephen Jones <srjones@mitre.org>
2014-10-15 14:03:27 -0400

    View emails when hovering over a username in the Analysts list so coordinators will know what organization they're from. Addressing issue #248

--- geoq/core/forms.py
+++ geoq/core/forms.py
@@ -5,6 +5,9 @@
 from django import forms
 from django.forms.widgets import (RadioInput, RadioSelect, CheckboxInput,
     CheckboxSelectMultiple)
+from django.contrib.auth.models import User
+from django.utils.html import escape, conditional_escape
+from itertools import chain
 from models import AOI, Job, Project
 
 no_style = [RadioInput, RadioSelect, CheckboxInput, CheckboxSelectMultiple]
@@ -31,12 +34,56 @@ class AOIForm(StyledModelForm):
                   'priority', 'status')
         model = AOI
 
+class AnalystSelectWidget(forms.SelectMultiple):
+    def __init__(self, attrs=None, choices=(), option_title_field=''):
+        self.option_title_field = option_title_field
+        super(AnalystSelectWidget, self).__init__(attrs, choices)
+
+    def render_option(self, selected_choices, option_value, option_label, option_title=''):
+        option_value = forms.util.force_text(option_value)
+        if option_value in selected_choices:
+            selected_html = u' selected="selected"'
+            if not self.allow_multiple_selected:
+                selected_choices.remove(option_value)
+        else:
+            selected_html = ''
+
+        return u'<option title="%s" value="%s"%s>%s</option>' % ( \
+            escape(option_title), escape(option_value), selected_html, conditional_escape(forms.util.force_text(option_label)))
+
+    def render_options(self, choices, selected_choices):
+            # Normalize to strings.
+            selected_choices = set(forms.util.force_text(v) for v in selected_choices)
+            choices = [(c[0], c[1], '') for c in choices]
+            more_choices = [(c[0], c[1]) for c in self.choices]
+            try:
+                option_title_list = [val_list[0] for val_list in self.choices.queryset.values_list(self.option_title_field)]
+                if len(more_choices) > len(option_title_list):
+                    option_title_list = [''] + option_title_list # pad for empty label field
+                more_choices = [(c[0], c[1], option_title_list[more_choices.index(c)]) for c in more_choices]
+            except:
+                more_choices = [(c[0], c[1], '') for c in more_choices] # couldn't get title values
+            output = []
+            for option_value, option_label, option_title in chain(more_choices, choices):
+                if isinstance(option_label, (list, tuple)):
+                    output.append(u'<optgroup label="%s">' % escape(forms.util.force_text(option_value)))
+                    for option in option_label:
+                        output.append(self.render_option(selected_choices, *option, **dict(option_title=option_title)))
+                    output.append(u'</optgroup>')
+                else: # option_label is just a string
+                    output.append(self.render_option(selected_choices, option_value, option_label, option_title))
+            return u'\n'.join(output)
 
 class JobForm(StyledModelForm):
+    analysts = forms.ModelMultipleChoiceField(
+        queryset = User.objects.all(),
+        widget = AnalystSelectWidget(option_title_field='email')
+    )
+
     class Meta:
 
-        fields = ('name', 'description', 'project',
-                  'analysts', 'teams', 'reviewers', 'feature_types', 'required_courses', 'map', 'tags')
+        fields = ('name', 'description', 'project', 'analysts',
+                  'teams', 'reviewers', 'feature_types', 'required_courses', 'map', 'tags')
         model = Job
 
     def __init__(self, project, *args, **kwargs):
@@ -50,6 +97,11 @@ class JobForm(StyledModelForm):
         remove_anonymous('reviewers')
         remove_anonymous('analysts')
         self.fields['project'].initial = project
+
+        if 'data' in kwargs:
+            self.fields['analysts'].initial = kwargs['data'].getlist('analysts',None)
+        else:
+            self.fields['analysts'].initial = kwargs['instance'].analysts.all().values_list('id', flat=True)
 
 
 class ProjectForm(StyledModelForm):
