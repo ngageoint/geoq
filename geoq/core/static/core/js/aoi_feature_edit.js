7c9fb7bb898d90b2c7935a1b212368b9e8b309f1
Stephen Jones <srjones@mitre.org>
2015-02-26 11:53:12 -0500

    increase zoom factor for inspecting building interiors. This should be settable in settings.py, but doesn't seem to be getting set in nginx. Addresses #294

--- geoq/core/static/core/js/aoi_feature_edit.js
+++ geoq/core/static/core/js/aoi_feature_edit.js
@@ -515,7 +515,8 @@ aoi_feature_edit.map_init = function (map, bounds) {
         aoi_feature_edit.watch_layer(e.layer, false);
     });
 
-    // map.options.maxZoom = 19;
+    // for some reason nginx isn't keeping MAX_ZOOM defined in settings.py. TODO: Figure out why
+    map.options.maxZoom = 21;
 
     var custom_map = aoi_feature_edit.aoi_map_json || {};
     aoi_feature_edit.map = map;
