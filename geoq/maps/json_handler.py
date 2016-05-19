91d54cb8b3f0d5087538b61c0a659adb83e5e12b
Rocca <DROCCA@MITRE.ORG>
2016-05-19 15:48:08 -0400

    debuging data

--- geoq/maps/json_handler.py
+++ geoq/maps/json_handler.py
@@ -1,7 +1,9 @@
 from json import load
+from pprint import pprint
 
 def handleUploadedJSON(f):
     with open(f, 'wb+') as data_file:
         data = load(data_file)
         jsonDictionary = {"name":data["name"], "type":data["type"], "url":data["url"]}
+        pprint(data)
         return jsonDictionary
