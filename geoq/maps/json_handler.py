d078cba64cd7d082a0ec4fc597093e8c1b68129d
Rocca <DROCCA@MITRE.ORG>
2016-05-20 08:13:42 -0400

    Attempt at file opening bug

--- geoq/maps/json_handler.py
+++ geoq/maps/json_handler.py
@@ -2,8 +2,7 @@ from json import load
 from pprint import pprint
 
 def handleUploadedJSON(f):
-    with open(f, 'wb+') as data_file:
-        data = load(data_file)
-        jsonDictionary = {"name":data["name"], "type":data["type"], "url":data["url"]}
-        pprint(data)
-        return jsonDictionary
+    data = load(f)
+    jsonDictionary = {"name":data["name"], "type":data["type"], "url":data["url"]}
+    pprint(data)
+    return jsonDictionary
