0374ae9cf79516d58861d8ff81b229c14b1334e5
Rocca <DROCCA@MITRE.ORG>
2016-05-20 09:10:42 -0400

    Attempt at adding JSON Validation

--- geoq/maps/json_handler.py
+++ geoq/maps/json_handler.py
@@ -2,6 +2,10 @@ from json import load
 from pprint import pprint
 
 def handleUploadedJSON(f):
-    data = load(f)
+    try:
+        data = load(f)
+    except ValueError as e:
+        ## bad json
+        return None;
     jsonDictionary = {"name":data["name"], "type":data["type"], "url":data["url"]}
     return jsonDictionary
