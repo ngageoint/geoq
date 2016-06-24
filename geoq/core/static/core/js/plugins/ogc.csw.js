df4912801c9357b346466eec70990eb3935361d6
Stephen Jones <srjones@mitre.org>
2016-06-24 10:44:07 -0400

    Add wmsUrl

--- geoq/core/static/core/js/plugins/ogc.csw.js
+++ geoq/core/static/core/js/plugins/ogc.csw.js
@@ -17,14 +17,14 @@ ogc_csw.current_layer_list = [];
 
 ogc_csw.schema = {
     'image_id': { key: 'dc:identifier', default: 'unknown'},
-    'wms': { key: 'dct:references', default: null },
+    'wmsUrl': { key: 'dct:references', default: null },
     'ObservationDate': { key: null, default: moment().format('YYYY-MM-DD')},
     'maxCloudCoverPercentageRate': { key: null, default: 1},
     'platformCode': { key: null, default: 'abc123'},
     'sensor': {key: null, default: 'def456'},
     'nef_name': {key: null, default: 'unknown'},
     'layerName': {key: 'dc:title', default: 'Unknown'},
-    'status': {key: null, default: 'Accepted'}
+    'status': {key: null, default: 'NotEvaluated'}
 };
 
 ogc_csw.init = function(options) {
@@ -120,7 +120,7 @@ ogc_csw.parseCSWRecord = function(record) {
         oRecord.lc = $box.filterNode('ows:LowerCorner').text();
 
         oRecord.options.image_id = ogc_csw.getRecordValue(record, 'image_id');
-        oRecord.options.wms = ogc_csw.getRecordValue(record, 'wms');
+        oRecord.options.wmsUrl = ogc_csw.getRecordValue(record, 'wmsUrl');
         oRecord.options.ObservationDate = ogc_csw.getRecordValue(record, 'ObservationDate');
         oRecord.options.maxCloudCoverPercentageRate = ogc_csw.getRecordValue(record, 'maxCloudCoverPercentageRate');
         oRecord.options.platformCode = ogc_csw.getRecordValue(record, 'platformCode');
