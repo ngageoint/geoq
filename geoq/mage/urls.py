9637e8659ba7b0d188454dc262f963a3552658fd
Stephen Jones <srjones@mitre.org>
2016-09-21 10:37:47 -0400

    allow spaces in icon names

--- geoq/mage/urls.py
+++ geoq/mage/urls.py
@@ -6,5 +6,5 @@ from views import mage_login, get_events, get_observations, get_icon
 urlpatterns = patterns('',
     url(r'^api/events/?$', get_events, name='get-events'),
     url(r'^api/events/(?P<id>\d+)/observations/?$', get_observations, name='get-observations'),
-    url(r'^api/events/(?P<id>\d+)/form/icons/(?P<type>\S+)/?$', get_icon, name='get-icon'),
+    url(r'^api/events/(?P<id>\d+)/form/icons/(?P<type>[ \S]+)/?$', get_icon, name='get-icon'),
 )
