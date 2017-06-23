38b96986c619f07a642ccdc728e057b422be4b12
stephenrjones <srjones@mitre.org>
2017-06-23 08:30:12 -0400

    Initial docker files

--- geoq/settings.py
+++ geoq/settings.py
@@ -322,6 +322,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+LOGGING_CONFIG = None
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
