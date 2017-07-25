3535bd670333bf18645b920e214d09b8238b73a3
stephenrjones <srjones@mitre.org>
2017-07-25 15:02:46 -0400

    revert

--- geoq/settings.py
+++ geoq/settings.py
@@ -204,7 +204,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
+                'django.contrib.messages.context_processors.messages',
+                'geoq.core.contextprocessors.app_settings'
         ]
     }
 
