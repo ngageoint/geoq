f1d791086bfc05bc44b54a69453abba9d08afd1c
stephenrjones <srjones@mitre.org>
2017-10-17 13:37:06 -0400

    clean up startup script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,5 @@
 export DJANGO_SETTINGS_MODULE=geoq.settings
 export PATH=$PATH:/usr/lib/postgresql/9.4/bin
 
-python manage.py migrate
-paver install_dev_fixtures
-
 
 python manage.py runserver 0.0.0.0:8000
